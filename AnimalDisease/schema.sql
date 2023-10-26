c905f7d01ae82299738bb2f52188d3e8837373ae
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:52:11 +0200

    test of SQL export

--- AnimalDisease/schema.sql
+++ AnimalDisease/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model AnimalDisease of the subject dataModel.Agrifood for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AnimalDisease_type AS ENUM ('AnimalDisease');
-CREATE TABLE AnimalDisease (address JSON, alternateName TEXT, animals JSON, areaServed TEXT, dataProvider TEXT, date TIMESTAMP, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, diagnosticTest TEXT, disease TEXT, name TEXT, owner JSON, source TEXT, type AnimalDisease_type, veterinarian TEXT, veterinarianTreatment TEXT);
+CREATE TABLE AnimalDisease (address JSON, alternateName TEXT, animals JSON, areaServed TEXT, dataProvider TEXT, date TIMESTAMP, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, diagnosticTest TEXT, disease TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type AnimalDisease_type, veterinarian TEXT, veterinarianTreatment TEXT);
