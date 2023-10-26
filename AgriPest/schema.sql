af2e4e936e7328cece1bc522e2aad8ecf3838376
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:51:55 +0200

    test of SQL export

--- AgriPest/schema.sql
+++ AgriPest/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model AgriPest of the subject dataModel.Agrifood for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AgriPest_type AS ENUM ('AgriPest');
-CREATE TABLE AgriPest (agroVocConcept TEXT, alternateName TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasAgriProductType JSON, name TEXT, owner JSON, relatedSource JSON, source TEXT, type AgriPest_type);
+CREATE TABLE AgriPest (agroVocConcept TEXT, alternateName TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasAgriProductType JSON, id TEXT PRIMARY KEY, name TEXT, owner JSON, relatedSource JSON, seeAlso JSON, source TEXT, type AgriPest_type);
