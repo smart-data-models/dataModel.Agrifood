fefd166a08aa162949c7255f8e6cc8c2dc05b2a6
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:51:36 +0200

    test of SQL export

--- AgriApp/schema.sql
+++ AgriApp/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model AgriApp of the subject dataModel.Agrifood for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE AgriApp_type AS ENUM ('AgriApp');
-CREATE TABLE AgriApp (address JSON, alternateName TEXT, areaServed TEXT, category JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, endpoint TEXT, name TEXT, owner JSON, relatedSource JSON, source TEXT, type AgriApp_type, version TEXT);
+CREATE TABLE AgriApp (address JSON, alternateName TEXT, areaServed TEXT, category JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, endpoint TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, relatedSource JSON, seeAlso JSON, source TEXT, type AgriApp_type, version TEXT);
