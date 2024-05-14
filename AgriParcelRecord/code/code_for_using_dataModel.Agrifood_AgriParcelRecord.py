979e1d8f2b4440f192ded548cf8e8cc19d1b6ee6
Alberto Abella <alberto.abella@fiware.org>
2024-05-14 18:33:15 +0200

    created example of code

---
+++ AgriParcelRecord/code/code_for_using_dataModel.Agrifood_AgriParcelRecord.py
@@ -0,0 +1,61 @@
+
+#         # This code allows you to install a orion-ld broker in a linux system
+#         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
+#         
+#         # INSTALL NGSI LD broker (OrionLD)
+#         sudo docker pull mongo:3.6
+#         sudo docker pull fiware/orion-ld
+#         sudo docker network create fiware_default
+#         sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles
+#         sudo docker run -d --name fiware-orionld -h orion --network=fiware_default -p 1026:1026  fiware/orion-ld -dbhost mongo-db
+#         
+#         # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#         sudo docker stop fiware-orionld
+#         sudo docker rm fiware-orionld
+#         sudo docker stop mongo-db
+#         sudo docker rm mongo-db
+#         sudo docker network rm fiware_default
+#         
+#         # CHECK INSTANCES
+#         # Check the broker is running
+#         curl -X GET 'http://localhost:1026/version'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#         
+#         # now the python code you can use to insert some value in the context broker according to the data model
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
+dataModel = "AgriParcelRecord"
+subject = "dataModel.Agrifood"
+atmosphericPressure = {'type': 'Property', 'value': 1013.25, 'unitCode': 'A97', 'observedAt': '2017-05-04T12:30:00Z'}
+attribute = "atmosphericPressure"
+value = atmosphericPressure
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+hasAgriParcel = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:AgriParcel:d3676010-d815-468c-9e01-25739c5a25ed'}"
+attribute = "hasAgriParcel"
+value = hasAgriParcel
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+hasDevice = {'type': 'Relationship', 'object': ['urn:ngsi-ld:Device:4a40aeba-4474-11e8-86bf-03d82e958ce6', 'urn:ngsi-ld:Device:63217d24-4474-11e8-9da2-c3dd3c36891b', 'urn:ngsi-ld:Device:68e091dc-4474-11e8-a398-df010c53b416', 'urn:ngsi-ld:6f44b54e-4474-11e8-8577-d7ff6a8ef551']}
+attribute = "hasDevice"
+value = hasDevice
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+leafRelativeHumidity = {'type': 'Property', 'value': 0.25, 'unitCode': 'P1', 'observedAt': '2017-05-04T12:30:00Z'}
+attribute = "leafRelativeHumidity"
+value = leafRelativeHumidity
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
