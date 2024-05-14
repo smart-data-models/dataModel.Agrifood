849d9d806f35ca0149e191c1b03d03ad7b771db7
Alberto Abella <alberto.abella@fiware.org>
2024-05-14 18:33:48 +0200

    created example of code

---
+++ Pen/code/code_for_using_dataModel.Agrifood_Pen.py
@@ -0,0 +1,61 @@
+
+#         # This code allows you to install a orion-ld broker in a linux system
+#         # The manuals are here https://github.com/FIWARE/context.Orion-LD/tree/develop/doc/manuals-ld
+#         
+#         # INSTALL NGSI LD broker (OrionLD)
+#         sudo docker pull mongo:3.6
+#         sudo docker pull fiware/orion-ld
+#         sudo docker network create fiware_default
+#         sudo docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles
+#         sudo docker run -d --name fiware-orionld -h orion --network=fiware_default -p 1026:1026  fiware/orion-ld -dbhost mongo-db
+#         
+#         # TO RELAUNCH (only if you have already installed a broker in the same machine)
+#         sudo docker stop fiware-orionld
+#         sudo docker rm fiware-orionld
+#         sudo docker stop mongo-db
+#         sudo docker rm mongo-db
+#         sudo docker network rm fiware_default
+#         
+#         # CHECK INSTANCES
+#         # Check the broker is running
+#         curl -X GET 'http://localhost:1026/version'
+#         
+#         # Check what entities are in the broker
+#         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
+#         
+#         # now the python code you can use to insert some value in the context broker according to the data model
+#         
+from pysmartdatamodels import pysmartdatamodels as sdm
+import subprocess
+serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
+dataModel = "Pen"
+subject = "dataModel.Agrifood"
+additionalInfo = {'type': 'Property', 'value': [{'name': 'Farm1FeedTray', 'value': 1}, {'name': 'Farm1DepartmentId', 'value': '43'}, {'name': 'Farm1ValveId', 'value': '69'}, {'name': 'Farm1PenId', 'value': '1'}, {'name': 'Farm1BuildingId', 'value': '2'}]}
+attribute = "additionalInfo"
+value = additionalInfo
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+buildingId = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:5ee3dbc8-343b-40a7-ac04-dec67215ff98'}"
+attribute = "buildingId"
+value = buildingId
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+companyId = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:4579b77f-31c1-44ef-b200-9a2407cc82e9'}"
+attribute = "companyId"
+value = companyId
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+compartmentId = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:ab8680c6-3e82-40fb-8577-f6a0ab717586'}"
+attribute = "compartmentId"
+value = compartmentId
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
+print(" Execute this instruction to check that the entities has been inserted")
+command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
