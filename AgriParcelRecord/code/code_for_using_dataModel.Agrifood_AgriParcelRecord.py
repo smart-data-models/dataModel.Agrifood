1515a6346ef4cf9aba9b27866a6e361d7dcab16d
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:17:42 +0200

    created example of code

--- AgriParcelRecord/code/code_for_using_dataModel.Agrifood_AgriParcelRecord.py
+++ AgriParcelRecord/code/code_for_using_dataModel.Agrifood_AgriParcelRecord.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "AgriParcelRecord"
 subject = "dataModel.Agrifood"
-atmosphericPressure = {'type': 'Property', 'value': 1013.25, 'unitCode': 'A97', 'observedAt': '2017-05-04T12:30:00Z'}
+atmosphericPressure = 1013.25
 attribute = "atmosphericPressure"
 value = atmosphericPressure
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hasAgriParcel = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:AgriParcel:d3676010-d815-468c-9e01-25739c5a25ed'}"
+hasAgriParcel = "urn:ngsi-ld:AgriParcel:d3676010-d815-468c-9e01-25739c5a25ed"
 attribute = "hasAgriParcel"
 value = hasAgriParcel
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hasDevice = {'type': 'Relationship', 'object': ['urn:ngsi-ld:Device:4a40aeba-4474-11e8-86bf-03d82e958ce6', 'urn:ngsi-ld:Device:63217d24-4474-11e8-9da2-c3dd3c36891b', 'urn:ngsi-ld:Device:68e091dc-4474-11e8-a398-df010c53b416', 'urn:ngsi-ld:6f44b54e-4474-11e8-8577-d7ff6a8ef551']}
+hasDevice = ['urn:ngsi-ld:Device:4a40aeba-4474-11e8-86bf-03d82e958ce6', 'urn:ngsi-ld:Device:63217d24-4474-11e8-9da2-c3dd3c36891b', 'urn:ngsi-ld:Device:68e091dc-4474-11e8-a398-df010c53b416', 'urn:ngsi-ld:6f44b54e-4474-11e8-8577-d7ff6a8ef551']
 attribute = "hasDevice"
 value = hasDevice
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-leafRelativeHumidity = {'type': 'Property', 'value': 0.25, 'unitCode': 'P1', 'observedAt': '2017-05-04T12:30:00Z'}
+leafRelativeHumidity = 0.25
 attribute = "leafRelativeHumidity"
 value = leafRelativeHumidity
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
